9c60cb0ba8e6d4c4fbbebaa20e271de24c7a3803
NA-Siddiky <siddiky.academic@gmail.com>
2022-09-30 01:20:59 +0600

    adding callback

--- server.js
+++ server.js
@@ -1,39 +1,57 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var app = express()
-var http = require('http').Server(app)
-var io = require('socket.io')(http)
-var mongoose = require('mongoose')
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var mongoose = require("mongoose");
 
-app.use(express.static(__dirname))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-var dbUrl = 'mongodb://NA-Siddiky:pust@2020@cluster0.7asqp.mongodb.net/?retryWrites=true&w=majority'
+var dbUrl =
+  //   "mongodb://NA-Siddiky:pust@2020@cluster0.7asqp.mongodb.net/?retryWrites=true&w=majority";
+  `mongodb+srv://NA-Siddiky:pust@2020@cluster0.ylija.mongodb.net/?retryWrites=true&w=majority`;
 
-var messages = [
-    { name: 'Nur', message: 'Hi' },
-    { name: 'Siddiky', message: 'Hello' }
-]
+var Message = mongoose.model("Message", {
+  name: String,
+  message: String,
+});
 
-app.get('/messages', (req, res) => {
-    res.send(messages)
-})
+app.get("/messages", (req, res) => {
+  Message.find({}, (err, messages) => {
+    res.send(messages);
+  });
+});
 
-app.post('/messages', (req, res) => {
-    messages.push(req.body)
-    io.emit('message', req.body)
-    res.sendStatus(200)
-})
+app.post("/messages", (req, res) => {
+  var message = new Message(req.body);
 
-io.on('connection', (socket) => {
-    console.log('a user connected')
-})
+  message.save((err) => {
+    if (err) sendStatus(500);
+
+    Message.findOne({ message: "badword" }, (err, censored) => {
+      if (censored) {
+        console.log("censored words found", censored);
+        Message.remove({ _id: censored.id }, (err) => {
+          console.log("removed censored message");
+        });
+      }
+    });
+
+    io.emit("message", req.body);
+    res.sendStatus(200);
+  });
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
 
 mongoose.connect(dbUrl, { useMongoClient: true }, (err) => {
-    console.log('mongo db connection', err)
-})
+  console.log("mongo db connection", err);
+});
 
 var server = http.listen(3500, () => {
-    console.log('server is listening on port', server.address().port)
-})
+  console.log("server is listening on port", server.address().port);
+});
